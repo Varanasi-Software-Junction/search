06aa38818b062777ce6d1c5990c276022a338cf0
champaksworldcreate <champaksworld@gmail.com>
2024-10-14 18:54:58 +0530

    Added a Speech Synthesizer

---
+++ vsj.js
@@ -0,0 +1,13 @@
+function getGreeting() {
+    const now = new Date();
+    const hour = now.getHours();
+  
+    if (hour < 12) {
+      return "Good morning";
+    } else if (hour < 18) {
+      return "Good afternoon";
+    } else {
+      return "Good evening";
+    }
+  }
+  
